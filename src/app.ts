d6d2db30381485ccffc2f5077238dec23661bc80
lucas <lucascpinheiro10@gmail.com>
2022-09-29 14:36:49 -0300

    refact: minor chages

--- src/app.ts
+++ src/app.ts
@@ -5,19 +5,20 @@ import ObjectModel, { mongoosePointModel } from './models/points.model'
 import Service from './services/points.service'
 import Controller from './controllers/points.controller'
 
-const PORT = 3000
+const PORT = Number(process.env.PORT) || 3000
+const MONGO_URI = process.env.MONGO_URI
 
 const app: Express = express()
 
 app.use(express.json())
 
-async function start () {
+export default async function start () {
   try {
     const db = new DB(new ObjectModel(mongoosePointModel), mongoose.connect)
     const service = new Service(db)
     const controller = new Controller(service)
 
-    await db.connect('mongodb://mongo/test')
+    await db.connect(MONGO_URI)
 
     app.get('/points', controller.handlerGETRequest.bind(controller))
     app.post('/points', controller.handlerPOSTRequest.bind(controller))
@@ -26,7 +27,6 @@ async function start () {
     })
   } catch (err) {
     console.log(err)
+    process.exit(1)
   }
 }
-
-start()
---
+++ src/server.ts
@@ -0,0 +1,4 @@
+import './config'
+import start from './app'
+
+start()
